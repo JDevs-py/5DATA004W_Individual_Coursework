a07513fc2ab9b00eb8c7cbd545646925d54f7129
JDevs-py <jerrydevelops03@gmail.com>
2025-05-06 10:07:31 +0000

    testing graphs

--- cw_app.py
+++ cw_app.py
@@ -10,7 +10,7 @@ df1 = pd.read_csv("1a_median_price_transposed.csv")
 df2 = pd.read_csv("1c_ratio_transposed.csv")
 
 st.bar_chart(df1)
-st.line_chart(df2)
+st.scatter_chart(df2)
 
 #st.dataframe(df1.head(10))
 #st.table(df2.head(10))
