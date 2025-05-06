8e4f6f9180d988f753449c5a64ef6843370b5c77
JDevs-py <jerrydevelops03@gmail.com>
2025-05-06 09:58:34 +0000

    changing code to display charts

--- cw_app.py
+++ cw_app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
 st.write("Hello streamlit")
 st.write("Welcome to the new dashboard")
+st.write("Beginning the dashboard")
+
+df1 = pd.read_csv("1a_median_price_transposed.csv")
+df2 = pd.read_csv("1c_ratio_transposed.csv")
+
+st.line_chart(df1)
+st.line_chart(df2)
+
+#st.dataframe(df1.head(10))
+#st.table(df2.head(10))
